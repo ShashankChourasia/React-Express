c89556a6b74e3390f59f02dd75fd7f1014c2835d
Shashank Chourasia <shashankchourasia1706@gmail.com>
2023-09-20 13:58:33 +0530

    router added

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css';
+import RootLayout from './pages/Root';
+import HomePage from './pages/Home';
+import BlogPage from './pages/Blogs';
+
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path:'blogs',
+        element: <BlogPage />
+      }
+    ]
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ blogs/src/pages/Blogs.js
@@ -0,0 +1,8 @@
+const BlogPage= () => {
+
+    return(
+        <h2>Blogs Page works</h2>
+    );
+};
+
+export default BlogPage;
